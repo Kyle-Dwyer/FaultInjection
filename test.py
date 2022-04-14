7b9eb2c803b0637fc2cd4917908f845269747fa5
Kyle-Dwyer <1005284123@qq.com>
2022-04-14 17:46:57 +0800

    submodule autoquery

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-import time
+import random
+from typing import Callable
+
+from main import *
 
 import yaml
 from kubernetes import client, config, utils
@@ -19,7 +22,7 @@ def apply_customer_object_from_file(yaml_file):
         dep = yaml.safe_load(f)
         api = client.CustomObjectsApi()
         resp = api.create_namespaced_custom_object(
-            group="Chaos",version="v1alpha1",plural="chaos",body=dep, namespace="ts")
+            group="Chaos", version="v1alpha1", plural="chaos", body=dep, namespace="ts")
         print("Deployment created. status='%s'" % resp.metadata.name)
 
 
@@ -29,7 +32,7 @@ def delete_customer_object_from_file(yaml_file):
         dep = yaml.safe_load(f)
         api = client.CustomObjectsApi()
         resp = api.delete_namespaced_custom_object(
-            group="Chaos",version="v1alpha1",plural="chaos",body=dep, namespace="ts")
+            group="Chaos", version="v1alpha1", plural="chaos", body=dep, namespace="ts")
         print("Deployment deleted. status='%s'" % resp.metadata.name)
 
 
@@ -69,9 +72,47 @@ pods_map = {'basic-network-delay': './chaos/network_delay/basic_network_delay.ym
             'travel-plan-http-outbound': './chaos/http_outbound/travel_plan_network_delay.yml',
             'user-http-outbound': './chaos/http_outbound/user_network_delay.yml'}
 
+def run(task: Callable):
+    task(10)
+
+def fun(a, b):
+    print(a+b)
+
+def test(inti):
+    d = [inti]
+    def tt(c):
+        d[0] += c
+        print(d[0])
+    return tt
+
 if __name__ == '__main__':
+    task = test(0)
+    run(task)
+    run(task)
+    exit(0)
+    s = "a_b_c_d"
+    print(time.strftime("%Y-%m-%d %H:%M:%S"))
+    exit(0)
+    print(random.sample(range(0, 24), 2))
+    aa = [5, 4, 1, 7, 6, 0, 3, 2]
+    b = [10, 13, 11, 9, 8, 12, 15, 14]
+    c = [21, 16, 23, 17, 18, 19, 22, 20]
+    last = -1
+    res = []
+    l = [a, b, c]
+    ii = [0, 0, 0]
+    for i in range(24):
+        r = random.randint(0, 2)
+        while r == last or ii[r] > 7:
+            r = random.randint(0, 2)
+        res.append(l[r][ii[r]])
+        ii[r] += 1
+        last = r
+    print(res)
+
     apply_customer_object_from_file(pods_map["basic-network-delay"])
     delete_customer_object_from_file(pods_map["basic-network-delay"])
+    time.sleep(200)
 # apply_from_single_file(pods_map['basic-network-delay'])
 # time.sleep(60)
 # delete_by_kind_and_name("basic-network-delay")
